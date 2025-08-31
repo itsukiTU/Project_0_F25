c662607f9999ca8a6d295ab90dc2e4fc8cf563ee
Itsuki Akiniwa <tuo88087@temple.edu>
2025-08-31 19:07:51 -0400

    fix: add function prototype to main file and update random.c logic

--- print_random_string.c
+++ print_random_string.c
@@ -1,37 +1,30 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 
+void rand_string(char *str, size_t size); 
 
-// random string generator wrapper function
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
      if (s) {
-         rand_string(s, size); // to be developed by the student
+         rand_string(s, size);
      }
      return s;
 }
 
-
-
 int main()
-t
 {
     char *str;
-    size_t size =   ;  //specify a value for size of word
+    size_t size = 12;
 
-	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
 
 	printf("Today's random word: ");
-    str = rand_string_alloc(size );
+    str = rand_string_alloc(size);
     printf("%s",str);
     free (str);
     
-
 	return(0);
 }
